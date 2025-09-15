aa8772d467b09dc915a1125d9cd8f381f1a800f8
KebinCo <jamesalastorbannister1539@gmail.com>
2025-09-15 16:03:58 +0900

    Update bee.js

--- bee.js
+++ bee.js
@@ -1,13 +1,6 @@
 $(function () {
-    $(".menu-link").click(function () {
-        $(".menu-link").removeClass("is-active");
-        $(this).addClass("is-active");
-    });
-});
-
-$(function () {
-    $(".main-header-link").click(function () {
-        $(".main-header-link").removeClass("is-active");
+    $(".dropdowns").click(function () {
+        $(".dropdowns").removeClass("is-active");
         $(this).addClass("is-active");
     });
 });
@@ -56,4 +49,61 @@ $(function () {
     $(".pop-up .close").click(function () {
         $(".overlay-app").removeClass("is-active");
     });
+});
+
+// ---------Responsive-navbar-active-animation----------
+function test() {
+    var tabsNewAnim = $('#navbarSupportedContent');
+    var selectorNewAnim = $('#navbarSupportedContent').find('.mx-auto li').length;
+    var activeItemNewAnim = tabsNewAnim.find('.mx-auto .active');
+    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    var itemPosNewAnimTop = activeItemNewAnim.position();
+    var itemPosNewAnimLeft = activeItemNewAnim.position();
+    $(".hori-selector").css({
+        "top": itemPosNewAnimTop.top + "px",
+        "left": itemPosNewAnimLeft.left + "px",
+        "height": activeWidthNewAnimHeight + "px",
+        "width": activeWidthNewAnimWidth + "px"
+    });
+    $("#navbarSupportedContent").on("click", ".mx-auto li", function (e) {
+        $('#navbarSupportedContent .mx-auto li').removeClass("active");
+        $(this).addClass('active');
+        var activeWidthNewAnimHeight = $(this).innerHeight();
+        var activeWidthNewAnimWidth = $(this).innerWidth();
+        var itemPosNewAnimTop = $(this).position();
+        var itemPosNewAnimLeft = $(this).position();
+        $(".hori-selector").css({
+            "top": itemPosNewAnimTop.top + "px",
+            "left": itemPosNewAnimLeft.left + "px",
+            "height": activeWidthNewAnimHeight + "px",
+            "width": activeWidthNewAnimWidth + "px"
+        });
+    });
+}
+$(document).ready(function () {
+    setTimeout(function () {
+        test();
+    });
+});
+$(window).on('resize', function () {
+    setTimeout(function () {
+        test();
+    }, 500);
+});
+$(".navbar-toggler").click(function () {
+    $(".navbar-collapse").slideToggle(300);
+    setTimeout(function () {
+        test();
+    });
+});
+
+// Add active class on another page linked
+jQuery(document).ready(function ($) {
+    var path = window.location.pathname.split("/").pop();
+    if (path == '') {
+        path = 'index.html';
+    }
+    var target = $('#navbarSupportedContent ul.mx-auto li a[href=\"' + path + '\"]');
+    target.parent().addClass('active');
 });
